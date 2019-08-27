12a6664f4ede1111544688eda316263e0b3a461c
abhijeet <abhijeet40308@gmail.com>
2019-08-27 05:45:49 +0530

    Updates

---
+++ precache-manifest.b7c3e2074242e9cde5bdfdc8909140d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7d4a8b620693c184670fbfa6c7e0483",
+    "url": "/instagram-clone/index.html"
+  },
+  {
+    "revision": "36271045e6d4436b7752",
+    "url": "/instagram-clone/static/css/main.a370d58c.chunk.css"
+  },
+  {
+    "revision": "cae8ebefcfd0e01f9154",
+    "url": "/instagram-clone/static/js/2.ee0cb06b.chunk.js"
+  },
+  {
+    "revision": "36271045e6d4436b7752",
+    "url": "/instagram-clone/static/js/main.ae9dba7f.chunk.js"
+  },
+  {
+    "revision": "2ec1804d7c0b33ad9a9d",
+    "url": "/instagram-clone/static/js/runtime~main.fefb9568.js"
+  },
+  {
+    "revision": "65ffa5ba8be30c2385827338d50a227b",
+    "url": "/instagram-clone/static/media/sprite.65ffa5ba.png"
+  }
+]);
