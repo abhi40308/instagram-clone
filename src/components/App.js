48322f39ba2b9b0ba0f46ba41cb5c493d045cf60
abhijeet <abhijeet40308@gmail.com>
2020-05-01 15:06:29 +0530

    image url check for validity

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,37 @@
-import React, { useState } from "react";
-import "../styles/App.css";
-import Header from "./Header.js";
-import Post from "./Post.js";
-import Profile from "./Profile.js";
-import Feed from "./Feed.js";
-import Upload from "./Upload.js"
+import React, { useState } from 'react';
+import '../styles/App.css';
+import Header from './Header.js';
+import Post from './Post.js';
+import Profile from './Profile.js';
+import Feed from './Feed.js';
+import Upload from './Upload.js';
 
 // for authentication using auth0
-import { useAuth0 } from "../auth/react-auth0-wrapper";
+import { useAuth0 } from '../auth/react-auth0-wrapper';
 
 // for routing
-import { Switch, Route } from "react-router-dom";
-import SecuredRoute from "./SecuredRoute";
+import { Switch, Route } from 'react-router-dom';
+import SecuredRoute from './SecuredRoute';
 
 // for apollo client
-import { ApolloProvider } from "@apollo/react-hooks";
-import { ApolloClient, HttpLink, InMemoryCache } from "apollo-boost";
-import { setContext } from "apollo-link-context";
+import { ApolloProvider } from '@apollo/react-hooks';
+import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-boost';
+import { setContext } from 'apollo-link-context';
+
+// for toast notifications
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
-
   const { isAuthenticated, user } = useAuth0();
 
   // used state to get accessToken through getTokenSilently(), the component re-renders when state changes, thus we have
   // our accessToken in apollo client instance.
-  const [accessToken, setAccessToken] = useState("");
+  const [accessToken, setAccessToken] = useState('');
 
   const { getTokenSilently, loading } = useAuth0();
   if (loading) {
-    return "Loading...";
+    return 'Loading...';
   }
 
   // get access token
@@ -46,7 +49,7 @@ function App() {
 
   // for apollo client
   const httpLink = new HttpLink({
-    uri: "https://instagram-clone-3.herokuapp.com/v1/graphql"
+    uri: 'https://instagram-clone-3.herokuapp.com/v1/graphql'
   });
 
   const authLink = setContext((_, { headers }) => {
@@ -72,14 +75,17 @@ function App() {
     cache: new InMemoryCache()
   });
 
+  // used for toast notifications
+  toast.configure();
+
   return (
     <ApolloProvider client={client}>
       <Header />
       {isAuthenticated && <Upload />}
       <Switch>
-        <Route exact path="/" component={Feed} />
-        <Route path={"/post/:id"} component={Post} />
-        <SecuredRoute path={"/user/:id"} component={Profile} />
+        <Route exact path='/' component={Feed} />
+        <Route path={'/post/:id'} component={Post} />
+        <SecuredRoute path={'/user/:id'} component={Profile} />
       </Switch>
     </ApolloProvider>
   );
--- src/components/Upload.js
+++ src/components/Upload.js
@@ -1,11 +1,12 @@
-import React, { useState } from "react";
-import "../styles/App.css";
-import { gql } from "apollo-boost";
-import { useMutation } from "@apollo/react-hooks";
-import { Container, Modal, Button, Row } from "react-bootstrap";
-import { useAuth0 } from "../auth/react-auth0-wrapper";
-import { POST_LIST } from "./Feed.js";
-import { USER_INFO } from "./Profile.js";
+import React, { useState } from 'react';
+import '../styles/App.css';
+import { gql } from 'apollo-boost';
+import { useMutation } from '@apollo/react-hooks';
+import { Container, Modal, Button, Row } from 'react-bootstrap';
+import { useAuth0 } from '../auth/react-auth0-wrapper';
+import { toast } from 'react-toastify';
+import { POST_LIST } from './Feed.js';
+import { USER_INFO } from './Profile.js';
 
 const SUBMIT_POST = gql`
   mutation($url: String!, $userId: String!, $caption: String!) {
@@ -20,58 +21,67 @@ function Upload() {
 
   const { user } = useAuth0();
 
-  const [caption, setCaption] = useState("");
-  const [url, setUrl] = useState("");
-  const [error, setError] = useState("");
+  const [caption, setCaption] = useState('');
+  const [url, setUrl] = useState('');
+  const [error, setError] = useState('');
 
   const [submitPost] = useMutation(SUBMIT_POST);
 
   return (
     <>
       <button
-        className="button-nodec post-upload-button"
+        className='button-nodec post-upload-button'
         onClick={() => setModalShow(true)}
       />
 
       <Modal
         show={modalShow}
         onHide={() => setModalShow(false)}
-        size="lg"
-        aria-labelledby="contained-modal-title-vcenter"
+        size='lg'
+        aria-labelledby='contained-modal-title-vcenter'
         centered
       >
         <Modal.Header closeButton>
-          <Modal.Title id="contained-modal-title-vcenter">Upload</Modal.Title>
+          <Modal.Title id='contained-modal-title-vcenter'>Upload</Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <Container>
             <form
               onSubmit={e => {
                 e.preventDefault();
-                submitPost({
-                  variables: { caption, userId: user.sub, url },
-                  refetchQueries: [
-                    { query: POST_LIST },
-                    { query: USER_INFO, variables: { id: user.sub } }
-                  ]
-                }).catch(function(error) {
-                  console.log(error);
-                  setError(error.toString());
-                });
+                // checks if the url points to a valid image
+                let img = new Image();
+                img.onload = function() {
+                  submitPost({
+                    variables: { caption, userId: user.sub, url },
+                    refetchQueries: [
+                      { query: POST_LIST },
+                      { query: USER_INFO, variables: { id: user.sub } }
+                    ]
+                  }).catch(function(error) {
+                    console.log(error);
+                    setError(error.toString());
+                  });
+                };
+                img.onerror = function() {
+                  toast(`No image exists at the given url`);
+                };
+                img.src = url; // fires off loading of image
+
                 //You are having a controlled component where input value is determined by this.state._variable_name.
                 // So once you submit you have to clear your state which will clear your input automatically.
-                setCaption("");
-                setUrl("");
+                setCaption('');
+                setUrl('');
               }}
             >
               <Row>
-                <span>Image Url (file upload is not supported currently):</span>
+                <span>Enter a valid Image Url:</span>
               </Row>
               <Row>
                 <input
                   value={url}
                   onChange={e => setUrl(e.target.value)}
-                  type="text"
+                  type='text'
                 />
               </Row>
               <Row>
@@ -81,15 +91,15 @@ function Upload() {
                 <input
                   value={caption}
                   onChange={e => setCaption(e.target.value)}
-                  type="text"
+                  type='text'
                 />
               </Row>
               <Row>
                 <Button
-                  variant="outline-dark"
-                  className="profile-logout top-padding"
-                  type="submit"
-                  value="Submit"
+                  variant='outline-dark'
+                  className='profile-logout top-padding'
+                  type='submit'
+                  value='Submit'
                   onClick={() => setModalShow(false)}
                 >
                   Submit
